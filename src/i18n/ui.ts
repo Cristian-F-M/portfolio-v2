43096c7aa71127fd6a1ef8178077b29ddaafef5e
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-05-19 16:34:26 -0500

    Add i18n utils

---
+++ src/i18n/ui.ts
@@ -0,0 +1,11 @@
+export const languages = {
+	en: 'English',
+	es: 'Español'
+}
+
+export const defaultLang = 'en'
+
+export const ui = {
+	es: { title: 'Titulo en español ' },
+	en: { title: 'Title in english' }
+} as const
