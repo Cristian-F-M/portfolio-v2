7f5f87ab5f6587c507991f2215e8ee10f8376683
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-05-19 16:36:26 -0500

    Set configs for i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,19 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import { defaultLang, languages } from './src/i18n/ui';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
+  },
+  i18n: {
+    defaultLocale: defaultLang,
+    locales: Object.entries(languages).map(([key, _]) => key),
+    routing: {
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: true
+    },
   }
 });
