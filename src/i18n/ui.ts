825eef63cab5d71ff18e39b1b03f68b204c56372
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-05-21 16:34:55 -0500

    Add education translations

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -20,7 +20,11 @@ export const ui = {
 			'<p>Me gusta <strong>adquirir</strong> nuevos conocimientos y <strong>compartirlos</strong> con los demás. Disfruto explorando la programación y la tecnología, y actualmente soy <strong>trainee</strong> en el campo del <strong>desarrollo web</strong>. Mi objetivo es seguir especializándome y avanzar hacia senior en esta área.</p> <p>Participé en un evento de desarrollo web donde obtuve el <strong>segundo</strong> lugar. Durante este evento, desarrollé un <strong>sistema de gestión</strong> para una <strong>biblioteca</strong>, completando tanto el <strong>backend</strong> como el <strong>frontend</strong>, así como la configuración de la <strong>base de datos</strong> en un lapso de 8 horas.</p>',
 		'experience.title': 'Experiencia laboral',
 		'experience.wm.summary':
-			'Encargado de realizar pruebas de calidad a la aplicación <strong>Meters NG</strong>, verificando que todas sus funciones operaran correctamente. Esto incluía asegurar que los usuarios pudieran tomar lecturas, completar validaciones y emitir facturas sin problemas, además de revisar las configuraciones realizadas desde el portal web para garantizar una experiencia fluida en campo.'
+			'Encargado de realizar pruebas de calidad a la aplicación <strong>Meters NG</strong>, verificando que todas sus funciones operaran correctamente. Esto incluía asegurar que los usuarios pudieran tomar lecturas, completar validaciones y emitir facturas sin problemas, además de revisar las configuraciones realizadas desde el portal web para garantizar una experiencia fluida en campo.',
+      'education.title': 'Educación',
+      'education.sena-adso.certificate': 'Tecnología en Analisís y Desarrollo de Software',
+      'education.sena-fbpj.certificate': "Curso de fundamentos básicos en programación java",
+      'education.sena-acc.certificate': "Apropiación de conceptops en ciberseguridad"
 	},
 	en: {
 		'index.title':
@@ -35,6 +39,10 @@ export const ui = {
 		'about.text':
 			'<p>I enjoy <strong>acquiring</strong> new knowledge and <strong>sharing</strong> it with others. I enjoy exploring programming and technology, and I am currently a <strong>trainee</strong> in the field of <strong>web development</strong>. My goal is to continue specializing and move toward a senior position in this area.</p> <p>I participated in a web development event where I achieved <strong>second</strong> place. During this event, I developed a <strong>management system</strong> for a <strong>library</strong>, completing both the <strong>backend</strong> and the <strong>frontend</strong>, as well as setting up the <strong>database</strong> within an 8-hour timeframe.</p>',
 		'experience.title': 'Work experience',
-		'experience.wm.summary': 'Responsible for performing quality testing on the <strong>Meters NG</strong> application, verifying that all its functions were operating correctly. This included ensuring users could take readings, complete validations, and issue invoices without issues, as well as reviewing configurations made through the web portal to ensure a smooth experience in the field.'
+		'experience.wm.summary': 'Responsible for performing quality testing on the <strong>Meters NG</strong> application, verifying that all its functions were operating correctly. This included ensuring users could take readings, complete validations, and issue invoices without issues, as well as reviewing configurations made through the web portal to ensure a smooth experience in the field.',
+    'education.title': "Education",
+    'education.sena-adso.certificate': 'Technology in Software Analysis and Development',
+    'education.sena-fbpj.certificate': "Basic Fundamentals Course in Java Programming",
+    'education.sena-acc.certificate': "Appropriation of concepts in cybersecurity"
 	}
 } as const
