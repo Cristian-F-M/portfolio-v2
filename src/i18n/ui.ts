67580b627cd87273b8f4bb33d1d81774c0fe607a
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-05-21 14:34:46 -0500

    Add about translations

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -14,7 +14,10 @@ export const ui = {
 		'network.copy': 'Copiado al portapapeles',
 		'hero.description':
 			'Tecnologo en <Strong>analisis y desarrollo de software</Strong> enfocado en el desarrollo web.',
-		'hero.image.alt': 'Foto de Cristian Fernando Morales Diaz'
+		'hero.image.alt': 'Foto de Cristian Fernando Morales Diaz',
+		'about.title': 'Sobre mí',
+		'about.text':
+			'<p>Me gusta <strong>adquirir</strong> nuevos conocimientos y <strong>compartirlos</strong> con los demás. Disfruto explorando la programación y la tecnología, y actualmente soy <strong>trainee</strong> en el campo del <strong>desarrollo web</strong>. Mi objetivo es seguir especializándome y avanzar hacia senior en esta área.</p> <p>Participé en un evento de desarrollo web donde obtuve el <strong>segundo</strong> lugar. Durante este evento, desarrollé un <strong>sistema de gestión</strong> para una <strong>biblioteca</strong>, completando tanto el <strong>backend</strong> como el <strong>frontend</strong>, así como la configuración de la <strong>base de datos</strong> en un lapso de 8 horas.</p>'
 	},
 	en: {
 		'index.title':
@@ -24,6 +27,9 @@ export const ui = {
 		'network.copy': 'Copied to clipboard',
 		'hero.description':
 			'<Strong>Software analysis and development</Strong> technologist focused on web development.',
-		'hero.image.alt': 'Cristian Fernando Morales Diaz Photo'
+		'hero.image.alt': 'Cristian Fernando Morales Diaz Photo',
+		'about.title': 'About me',
+		'about.text':
+			'<p>I enjoy <strong>acquiring</strong> new knowledge and <strong>sharing</strong> it with others. I enjoy exploring programming and technology, and I am currently a <strong>trainee</strong> in the field of <strong>web development</strong>. My goal is to continue specializing and move toward a senior position in this area.</p> <p>I participated in a web development event where I achieved <strong>second</strong> place. During this event, I developed a <strong>management system</strong> for a <strong>library</strong>, completing both the <strong>backend</strong> and the <strong>frontend</strong>, as well as setting up the <strong>database</strong> within an 8-hour timeframe.</p>'
 	}
 } as const
