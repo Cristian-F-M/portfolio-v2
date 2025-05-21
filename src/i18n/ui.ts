93d86a3650280fcdd40f46f60f54e7438b8c34de
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-05-21 15:49:29 -0500

    Add experience translations

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -17,7 +17,10 @@ export const ui = {
 		'hero.image.alt': 'Foto de Cristian Fernando Morales Diaz',
 		'about.title': 'Sobre mí',
 		'about.text':
-			'<p>Me gusta <strong>adquirir</strong> nuevos conocimientos y <strong>compartirlos</strong> con los demás. Disfruto explorando la programación y la tecnología, y actualmente soy <strong>trainee</strong> en el campo del <strong>desarrollo web</strong>. Mi objetivo es seguir especializándome y avanzar hacia senior en esta área.</p> <p>Participé en un evento de desarrollo web donde obtuve el <strong>segundo</strong> lugar. Durante este evento, desarrollé un <strong>sistema de gestión</strong> para una <strong>biblioteca</strong>, completando tanto el <strong>backend</strong> como el <strong>frontend</strong>, así como la configuración de la <strong>base de datos</strong> en un lapso de 8 horas.</p>'
+			'<p>Me gusta <strong>adquirir</strong> nuevos conocimientos y <strong>compartirlos</strong> con los demás. Disfruto explorando la programación y la tecnología, y actualmente soy <strong>trainee</strong> en el campo del <strong>desarrollo web</strong>. Mi objetivo es seguir especializándome y avanzar hacia senior en esta área.</p> <p>Participé en un evento de desarrollo web donde obtuve el <strong>segundo</strong> lugar. Durante este evento, desarrollé un <strong>sistema de gestión</strong> para una <strong>biblioteca</strong>, completando tanto el <strong>backend</strong> como el <strong>frontend</strong>, así como la configuración de la <strong>base de datos</strong> en un lapso de 8 horas.</p>',
+		'experience.title': 'Experiencia laboral',
+		'experience.wm.summary':
+			'Encargado de realizar pruebas de calidad a la aplicación <strong>Meters NG</strong>, verificando que todas sus funciones operaran correctamente. Esto incluía asegurar que los usuarios pudieran tomar lecturas, completar validaciones y emitir facturas sin problemas, además de revisar las configuraciones realizadas desde el portal web para garantizar una experiencia fluida en campo.'
 	},
 	en: {
 		'index.title':
@@ -30,6 +33,8 @@ export const ui = {
 		'hero.image.alt': 'Cristian Fernando Morales Diaz Photo',
 		'about.title': 'About me',
 		'about.text':
-			'<p>I enjoy <strong>acquiring</strong> new knowledge and <strong>sharing</strong> it with others. I enjoy exploring programming and technology, and I am currently a <strong>trainee</strong> in the field of <strong>web development</strong>. My goal is to continue specializing and move toward a senior position in this area.</p> <p>I participated in a web development event where I achieved <strong>second</strong> place. During this event, I developed a <strong>management system</strong> for a <strong>library</strong>, completing both the <strong>backend</strong> and the <strong>frontend</strong>, as well as setting up the <strong>database</strong> within an 8-hour timeframe.</p>'
+			'<p>I enjoy <strong>acquiring</strong> new knowledge and <strong>sharing</strong> it with others. I enjoy exploring programming and technology, and I am currently a <strong>trainee</strong> in the field of <strong>web development</strong>. My goal is to continue specializing and move toward a senior position in this area.</p> <p>I participated in a web development event where I achieved <strong>second</strong> place. During this event, I developed a <strong>management system</strong> for a <strong>library</strong>, completing both the <strong>backend</strong> and the <strong>frontend</strong>, as well as setting up the <strong>database</strong> within an 8-hour timeframe.</p>',
+		'experience.title': 'Work experience',
+		'experience.wm.summary': 'Responsible for performing quality testing on the <strong>Meters NG</strong> application, verifying that all its functions were operating correctly. This included ensuring users could take readings, complete validations, and issue invoices without issues, as well as reviewing configurations made through the web portal to ensure a smooth experience in the field.'
 	}
 } as const
