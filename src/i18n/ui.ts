e086818ece09f26ca8ea9dbf5899ccecf4f76ef6
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-05-21 13:51:57 -0500

    Add translations

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -6,6 +6,24 @@ export const languages = {
 export const defaultLang = 'es'
 
 export const ui = {
-	es: { title: 'Titulo en español ' },
-	en: { title: 'Title in english' }
+	es: {
+		'index.title':
+			'Portafolio de Cristian Morales | Trainee en el desarrollo de software',
+		'index.description':
+			'Trainee en el desarrollo de software enfocado en el desarrollo web pero con habilidades en diferentes plataformas como Móvil, Escritorio.',
+		'network.copy': 'Copiado al portapapeles',
+		'hero.description':
+			'Tecnologo en <Strong>analisis y desarrollo de software</Strong> enfocado en el desarrollo web.',
+		'hero.image.alt': 'Foto de Cristian Fernando Morales Diaz'
+	},
+	en: {
+		'index.title':
+			"Cristian Morales's Portfolio | Trainee in Software Development",
+		'index.description':
+			'Trainee in software development focused on web development, with skills in various platforms such as Mobile, Desktop.',
+		'network.copy': 'Copied to clipboard',
+		'hero.description':
+			'<Strong>Software analysis and development</Strong> technologist focused on web development.',
+		'hero.image.alt': 'Cristian Fernando Morales Diaz Photo'
+	}
 } as const
