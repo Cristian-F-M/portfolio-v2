99ae5c7c06ac767a737b0b8cbffb6b3ba433f279
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-05-23 12:39:06 -0500

    Update "t" function to use new translations object

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,13 +1,17 @@
-import { ui, defaultLang } from './ui';
+import { ui, defaultLang } from './ui'
 
 export function getLangFromUrl(url: URL) {
-  const [, lang] = url.pathname.split('/');
-  if (lang in ui) return lang as keyof typeof ui;
-  return defaultLang;
+	const [, lang] = url.pathname.split('/')
+	if (lang in ui) return lang as keyof typeof ui
+	return defaultLang
 }
 
+type TFnType = (obj: (typeof ui)[keyof typeof ui]) => string
+
 export function useTranslations(lang: keyof typeof ui) {
-  return function t(key: keyof typeof ui[typeof defaultLang]) {
-    return ui[lang][key] || ui[defaultLang][key];
-  }
+	return function t(key: TFnType) {
+		const currentLang = structuredClone(ui[lang])
+		const defaultLangClone = structuredClone(ui[defaultLang])
+		return key(currentLang) || key(defaultLangClone)
+	}
 }
